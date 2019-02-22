ffb9eac12245a6fa78e42a506b1742247e1e4341
mcloots <michael.cloots@gmail.com>
2019-02-22 10:17:36 +0100

    a

--- HelloCore/Models/Bestelling.cs
+++ HelloCore/Models/Bestelling.cs
@@ -8,10 +8,13 @@ namespace HelloCore.Models
     {
         public int BestellingID { get; set; }
         public string Artikel { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public Decimal? Prijs { get; set; }
+        [Display(Name = "Klant")]
         public int KlantID {get; set; }
         public Klant Klant { get; set; }
 
+        [NotMapped]
         public string KlantDisplay {
             get {
                 if(Klant != null) {
--- HelloCore/Startup.cs
+++ HelloCore/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using HelloCore.Data;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
