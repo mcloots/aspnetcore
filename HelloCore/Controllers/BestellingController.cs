2ecbc45923f940bf1e406cb274e4906ca496347b
mcloots <michael.cloots@gmail.com>
2019-02-25 17:04:50 +0100

    bestellingen vervolledigd

--- HelloCore/Controllers/BestellingController.cs
+++ HelloCore/Controllers/BestellingController.cs
@@ -33,7 +33,8 @@ namespace HelloCore.Controllers
         {
             if (!string.IsNullOrEmpty(viewModel.ArtikelSearch))
             {
-                viewModel.Bestellingen = await _context.Bestellingen.Include(b => b.Klant).Where(b => b.Artikel.StartsWith(viewModel.ArtikelSearch)).ToListAsync();
+                viewModel.Bestellingen = await _context.Bestellingen.Include(b => b.Klant)
+                    .Where(b => b.Artikel.StartsWith(viewModel.ArtikelSearch)).ToListAsync();
             } else
             {
                 viewModel.Bestellingen = await _context.Bestellingen.Include(b => b.Klant).ToListAsync();
@@ -67,6 +68,112 @@ namespace HelloCore.Controllers
 
             viewmodel.Klanten = new SelectList(_context.Klanten, "KlantID", "Naam");
             return View(viewmodel);
+        }
+
+        // GET: Bestelling/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Bestelling = await _context.Bestellingen.FindAsync(id);
+            if (Bestelling == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.Klanten = new SelectList(_context.Klanten, "KlantID", "Naam");
+            return View(Bestelling);
+        }
+
+        // POST: Bestelling/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BestellingID,Artikel,KlantID,Prijs")] Bestelling Bestelling)
+        {
+            if (id != Bestelling.BestellingID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(Bestelling);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BestellingExists(Bestelling.BestellingID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Klanten = new SelectList(_context.Klanten, "KlantID", "Naam");
+            return View(Bestelling);
+        }
+
+        // GET: Bestelling/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Bestelling = await _context.Bestellingen.Include(b => b.Klant)
+                .FirstOrDefaultAsync(m => m.BestellingID == id);
+            if (Bestelling == null)
+            {
+                return NotFound();
+            }
+
+            return View(Bestelling);
+        }
+
+        // POST: Bestelling/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var Bestelling = await _context.Bestellingen.FindAsync(id);
+            _context.Bestellingen.Remove(Bestelling);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BestellingExists(int id)
+        {
+            return _context.Bestellingen.Any(e => e.BestellingID == id);
+        }
+
+        // GET: Bestelling/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var Bestelling = await _context.Bestellingen.Include(b=>b.Klant)
+                .FirstOrDefaultAsync(m => m.BestellingID == id);
+            if (Bestelling == null)
+            {
+                return NotFound();
+            }
+
+            return View(Bestelling);
         }
     }
 }
